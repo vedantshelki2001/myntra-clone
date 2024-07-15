0d044412789a874defcf602a92a307f2a62566c1
vedantshelki2001 <vedantshelki2001@gmail.com>
2024-07-15 17:21:51 -0400

    first myntra commit

---
+++ myntra/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className='text-blue-500 text-2xl'>Myntra Clone</h1>
+      <p>Myntra</p>
+    </div>
+  )
+}
+
+export default App
